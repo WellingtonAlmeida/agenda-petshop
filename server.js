6eb4b24685046feb738bb0280be2005de0d54ef8
Wellington Guimaraes de Almeida <walmeida@trt3.jus.br>
2019-08-08 07:38:16 -0300

    Aula 04

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
 const Operacoes = require('./infraestrutura/operations')
 const Cliente = new Operacoes('cliente')
+const Pets = new Operacoes('pet')
 
 conexao.connect(erro => {
   if (erro) {
@@ -18,12 +19,14 @@ const resolvers = {
   Query: {
     status: () => 'Servidor rodando!',
     clientes: () => Cliente.lista(),
-    cliente: (root, { id }) => Cliente.buscaPorId(id)
+    cliente: (root, { id }) => Cliente.buscaPorId(id),
+    pets: () => Pets.lista() 
   },
   Mutation: {
     adicionarCliente: (root, params) => Cliente.adiciona(params),
     atualizarCliente: (root, params) => Cliente.atualiza(params),
-    deletarCliente: (root, { id }) => Cliente.deleta(id)
+    deletarCliente: (root, { id }) => Cliente.deleta(id),
+    adicionarPet: (root, params) => Pets.adiciona(params)
   }
 }
 
